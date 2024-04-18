0e728bf884e5e2515425bb4c840e37b5e73c9681
Afonso Alcantara <alcantara.afonso@gmail.com>
2024-04-18 19:20:57 -0300

    services and interfaces

---
+++ README.md
@@ -0,0 +1,12 @@
+# Arquitetura Hexagonal em GO
+Este projeto visa ilustrar uma implementação da arquitetura hexagonal em GO
+
+## Execução
+Projeto é rodado em um container, logo, suba-o com
+```
+docker componse up
+```
+
+
+## Testes
+Execute o comando go test ./... (na exec do docker) para rodar os testes
--- application/product.go
+++ application/product.go
@@ -1,6 +1,16 @@
 package application
 
-import "errors"
+import (
+	"errors"
+
+	"github.com/asaskevich/govalidator"
+	uuid "github.com/satori/go.uuid"
+)
+
+//funcao init é executada antes da função main
+func init() {
+	govalidator.SetFieldsRequiredByDefault(true)
+}
 
 type ProductInterface interface {
 	IsValid() (bool, error)
@@ -12,23 +22,69 @@ type ProductInterface interface {
 	GetPrice() float64
 }
 
+type ProductServiceInterface interface {
+	Get(id string) (ProductInterface, error)
+	Create(product Product) (ProductInterface, error)
+	Enable(product ProductInterface) (ProductInterface, error)
+	Disable(product ProductInterface) (ProductInterface, error)
+}
+
+type ProductReader interface {
+	Get(id string) (ProductInterface, error)
+}
+
+type ProductWriter interface {
+	Save(product ProductInterface) (ProductInterface, error)
+}
+
+//Composição de interfaces
+type ProductPersistenceInterface interface {
+	ProductReader
+	ProductWriter
+}
+
 const (
 	DISABLED = "disabled"
 	ENABLED  = "enabled"
 )
 
 type Product struct {
-	Id     string
-	Name   string
-	Price  float64
-	Status string
+	Id     string  `valid:"uuidv4"`
+	Name   string  `valid:"required"`
+	Price  float64 `valid:"float,optional"`
+	Status string  `valid:"required"`
+}
+
+func NewProduct() *Product {
+	product := Product{
+		Id:     uuid.NewV4().String(),
+		Status: DISABLED,
+	}
+	return &product //Retorna um ponteiro (localização) para a struct Product
+
 }
 
 //A declaração dos métodos da interface ProductInterface
 //Isso significa que a struct Product implementa a interface ProductInterface
-// func (p *Product) IsValid() (bool, error) {
+func (p *Product) IsValid() (bool, error) {
+	if p.Status == "" {
+		p.Status = DISABLED
+	}
 
-// }
+	if p.Status != ENABLED && p.Status != DISABLED {
+		return false, errors.New("the status must be enabled or disabled")
+	}
+
+	if p.Price < 0 {
+		return false, errors.New("the price must be greater than zero")
+	}
+
+	_, err := govalidator.ValidateStruct(p)
+	if err != nil {
+		return false, err
+	}
+	return true, nil
+}
 
 func (p *Product) Enable() error {
 	if p.Price > 0 {
@@ -39,9 +95,13 @@ func (p *Product) Enable() error {
 	return errors.New("the price must be greater than zero to enable the product")
 }
 
-// func (p *Product) Disable() error {
-
-// }
+func (p *Product) Disable() error {
+	if p.Price == 0 {
+		p.Status = DISABLED
+		return nil
+	}
+	return errors.New("the price must be zero in order to have the product disabled")
+}
 
 func (p *Product) GetId() string {
 	return p.Id
